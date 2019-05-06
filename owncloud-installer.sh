3392e92150dd76effd5cc02efe8e7f10afcb805e
Momonosuke <ade.purnomo50@gmail.com>
2019-05-07 00:47:50 +0700

    Update owncloud-installer.sh

--- owncloud-installer.sh
+++ owncloud-installer.sh
@@ -29,86 +29,20 @@ echo "${hijau}Instlling docker + enable service..."
 /bin/systemctl enable docker.service > /dev/null 2>&1
 
 echo "${hijau}Create instansi..."  
+
 cd /opt > /dev/null 2>&1
 /bin/mkdir /root/owncloud-docker-server > /dev/null 2>&1
 chmod 777 /root/owncloud-docker-server > /dev/null 2>&1
-cd owncloud-docker-server > /dev/null 2>&1
+/bin/cd /opt/owncloud-docker-server/ > /dev/null 2>&1
+wget https://raw.githubusercontent.com/owncloud-docker/server/master/docker-compose.yml
 
-echo "${hijau}Create configuration for owncloud , *silahkan edit ..."
-
-/bin/cat << EOF >> /root/owncloud-docker-server/docker-compose.yml
-version: '2.1'
-
-volumes:
-  files:
-    driver: local
-  mysql:
-    driver: local
-  backup:
-    driver: local
-  redis:
-    driver: local
-
-services:
-  owncloud:
-    image: owncloud/server:${OWNCLOUD_VERSION}
-    restart: always
-    ports:
-      - ${HTTP_PORT}:8080
-    depends_on:
-      - db
-      - redis
-    environment:
-      - OWNCLOUD_DOMAIN=${OWNCLOUD_DOMAIN}
-      - OWNCLOUD_DB_TYPE=mysql
-      - OWNCLOUD_DB_NAME=owncloud
-      - OWNCLOUD_DB_USERNAME=owncloud
-      - OWNCLOUD_DB_PASSWORD=owncloud
-      - OWNCLOUD_DB_HOST=db
-      - OWNCLOUD_ADMIN_USERNAME=${ADMIN_USERNAME}
-      - OWNCLOUD_ADMIN_PASSWORD=${ADMIN_PASSWORD}
-      - OWNCLOUD_MYSQL_UTF8MB4=true
-      - OWNCLOUD_REDIS_ENABLED=true
-      - OWNCLOUD_REDIS_HOST=redis
-    healthcheck:
-      test: ["CMD", "/usr/bin/healthcheck"]
-      interval: 30s
-      timeout: 10s
-      retries: 5
-    volumes:
-      - files:/mnt/data
-
-  db:
-    image: webhippie/mariadb:latest
-    restart: always
-    environment:
-      - MARIADB_ROOT_PASSWORD=owncloud
-      - MARIADB_USERNAME=owncloud
-      - MARIADB_PASSWORD=owncloud
-      - MARIADB_DATABASE=owncloud
-      - MARIADB_MAX_ALLOWED_PACKET=128M
-      - MARIADB_INNODB_LOG_FILE_SIZE=64M
-    healthcheck:
-      test: ["CMD", "/usr/bin/healthcheck"]
-      interval: 30s
-      timeout: 10s
-      retries: 5
-    volumes:
-      - mysql:/var/lib/mysql
-      - backup:/var/lib/backup
-
-  redis:
-    image: webhippie/redis:latest
-    restart: always
-    environment:
-      - REDIS_DATABASES=1
-    healthcheck:
-      test: ["CMD", "/usr/bin/healthcheck"]
-      interval: 30s
-      timeout: 10s
-      retries: 5
-    volumes:
-      - redis:/var/lib/redis
+/bin/cd /opt/owncloud-docker-server/
+cat << EOF > .env
+OWNCLOUD_VERSION=10.0
+OWNCLOUD_DOMAIN=localhost
+ADMIN_USERNAME=admin
+ADMIN_PASSWORD=admin
+HTTP_PORT=80
 EOF
 chmod a+x /root/owncloud-docker-server/.env
 clear
