3756001bfb7b63a12296c07a4e4119818a1db1f0
Momonosuke <ade.purnomo50@gmail.com>
2019-05-16 20:53:22 +0700

    Update owncloud-installer.sh

--- owncloud-installer.sh
+++ owncloud-installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 ##
 hijau=$(tput setaf 2)
 echo "${hijau}######################################"
@@ -14,26 +14,21 @@ sed -i "s|#Banner none|Banner /etc/issue.net|" sshd_config
 chmod a+x /etc/ssh/sshd_config
 #service sshd restart
 rm -rf /root/test
-
 sleep 10
+echo "######################################"
 echo "${hijau}Instlling curl..."
 echo "######################################"
 yum install curl -y > /dev/null 2>&1
 echo "${hijau} download docker composer..please wait ..."
 /bin/curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
 chmod a+x /usr/local/bin/docker-compose > /dev/null 2>&1
-
-echo "${hijau}Instlling docker + enable service..."
+echo "######################################"
+echo "${hijau}Instlling docker..."
 echo "######################################"
 /bin/yum install docker -y > /dev/null 2>&1
-
-/bin/systemctl start docker.service > /dev/null 2>&1
-/bin/systemctl enable docker.service > /dev/null 2>&1
-
+echo "######################################"
 echo "${hijau}Create instansi..."  
 echo "######################################"
-
-cd /opt > /dev/null 2>&1
 mkdir /opt/owncloud-docker-server > /dev/null 2>&1
 chmod 777 /opt/owncloud-docker-server > /dev/null 2>&1
 ##########################################################################
@@ -123,12 +118,17 @@ HTTP_PORT=80
 EOF
 chmod a+x /opt/owncloud-docker-server/.env
 ##########################################################################
+/bin/systemctl start docker.service > /dev/null 2>&1
+/bin/systemctl enable docker.service > /dev/null 2>&1
+echo "----------------------------------------------------------------------"
 echo "${hijau}Downloading +compose file from source *Sabarr ya ganss ..."
-echo "######################################"
+echo "----------------------------------------------------------------------"
 docker-compose up 
+echo "######################################"
 echo "${hijau}Done ..."
 echo "${hijau}Login information"
 echo "${hijau}ADMIN_USERNAME=admin"
 echo "${hijau}ADMIN_PASSWORD=admin"
+echo "######################################"
 echo "${hijau}MANUAL INSTALATION https://www.marksei.com/install-owncloud-10-server-centos/" 
 	  
