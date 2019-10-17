09a0cc01dbdf325dcb265845dac3ce79ed29b74a
Momonosuke <ade.purnomo50@gmail.com>
2019-10-17 17:01:55 +0700

    Update owncloud-installer.sh

--- owncloud-installer.sh
+++ owncloud-installer.sh
@@ -145,7 +145,7 @@ yum update -y
 cd /opt/
 git clone https://github.com/netdata/netdata.git
 cd /opt/netdata
-sed -i 's/-eq 0/0/g' /opt/netdata/netdata-installer.sh
+sed -i 's/TWAIT} -eq 0/TWAIT}  0/g' /opt/netdata/netdata-installer.sh
 
 chmod 777 /opt/netdata/netdata-installer.sh
 ./netdata-installer.sh --auto-update
