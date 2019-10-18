d6cb38194d1a1b69938a3f8129daff32ae4f582d
Momonosuke <ade.purnomo50@gmail.com>
2019-10-18 13:30:52 +0700

    Update owncloud-installer.sh

--- owncloud-installer.sh
+++ owncloud-installer.sh
@@ -7,14 +7,14 @@ cd ~
 chmod 777 owncloud-installer.sh
 echo "${hijau}Please run this scripts on SU"
 echo "-------------------------------------------------"
-echo "${hijau}configure....."
+echo "${hijau}configure...please wait.."
 echo "-------------------------------------------------"
 
 hostnamectl set-hostname owncloud
-yum install git -y > /dev/null 2>&1
+yum install Judy-devel autoconf autoconf-archive autogen automake gcc git libmnl-devel libuuid-devel libuv-deve lz4-devel nmap-ncat openssl-devel zlib-devel git -y > /dev/null 2>&1
 
 cd ~
-git clone https://github.com/Adepurnomo/banner.git > /dev/null 2>&1
+git clone https://github.com/Adepurnomo/banner.git 
 \cp /root/banner/issue.net /etc
 chmod a+x /etc/issue.net
 cd /etc/ssh/ 	
@@ -22,23 +22,17 @@ sed -i "s|#Banner none|Banner /etc/issue.net|" sshd_config
 chmod a+x /etc/ssh/sshd_config
 rm -rf /root/banner
 
-sleep 10
 echo "-------------------------------------------------"
 echo "${hijau}Working....."
 echo "-------------------------------------------------"
 yum install wget -y > /dev/null 2>&1
-echo "-------------------------------------------------"
 echo "${hijau}get docker composer..please wait ..."
 echo "-------------------------------------------------"
 curl -L https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose > /dev/null 2>&1
 chmod +x /usr/local/bin/docker-compose
-#curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
-#chmod a+x /usr/local/bin/docker-compose > /dev/null 2>&1
-echo "-------------------------------------------------"
 echo "${hijau}Installing docker..."
 echo "-------------------------------------------------"
 yum install docker -y > /dev/null 2>&1
-echo "-------------------------------------------------"
 echo "${hijau}Create instance..."  
 echo "-------------------------------------------------"
 
@@ -124,14 +118,11 @@ HTTP_PORT=80
 EOF
 chmod 777 /opt/owncloud-docker-server/.env
 
-systemctl start docker.service > /dev/null 2>&1
-systemctl enable docker.service > /dev/null 2>&1
 echo "----------------------------------------------------------------------"
 echo "${hijau}Downloading +compose file from source *Sabarr ya ganss ..."
 echo "----------------------------------------------------------------------"
 
 #for netdata
-yum install Judy-devel autoconf autoconf-archive autogen automake gcc git libmnl-devel libuuid-devel libuv-deve lz4-devel nmap-ncat openssl-devel zlib-devel -y
 cd /opt/
 git clone https://github.com/netdata/netdata.git
 cd /opt/netdata
@@ -140,19 +131,23 @@ sed -i 's/TWAIT} -eq 0/TWAIT} -eq 1/g' /opt/netdata/netdata-installer.sh
 chmod 777 /opt/netdata/netdata-installer.sh
 ./netdata-installer.sh --auto-update
 
+systemctl start docker.service > /dev/null 2>&1
+systemctl enable docker.service > /dev/null 2>&1
 cd /opt/owncloud-docker-server/
 docker-compose up -d
 
 cd /etc/sysconfig
 setenforce 0
 sed -i "s|SELINUX=enforcing|SELINUX=disabled|" selinux
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --zone=public --add-port=443/tcp --permanent
-firewall-cmd --zone=public --add-port=8080/tcp --permanent
-firewall-cmd --zone=public --add-port=19999/tcp --permanent
-firewall-cmd --reload
+firewall-cmd --zone=public --add-port=80/tcp --permanent > /dev/null 2>&1
+firewall-cmd --zone=public --add-port=443/tcp --permanent > /dev/null 2>&1
+firewall-cmd --zone=public --add-port=8080/tcp --permanent > /dev/null 2>&1
+firewall-cmd --zone=public --add-port=19999/tcp --permanent > /dev/null 2>&1
+firewall-cmd --reload > /dev/null 2>&1
 cd ~
-
+systemctl restart docker > /dev/null 2>&1
+systemctl restart netadata > /dev/null 2>&1
+ 
 echo "----------------------------------------------------------------------"
 echo "${hijau}Done ..."
 host=$(hostname -I)
